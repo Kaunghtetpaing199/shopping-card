77ee29f07e89fe678e2ff23597e4a2aac9c41402
Kaung Htet Paing <kaunghtetpaing199@gmail.com>
2021-08-22 21:55:35 +0630

    edit equation

--- src/Card Component/home.js
+++ src/Card Component/home.js
@@ -4,39 +4,39 @@ import "./style.css";
 import Modal from "./cart/modal";
 import Cart from "./cart/shopping-cart";
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      beachData: Data,
-      cardData: [],
-      appendCard: false
-    };
-  }
-  showClasses = () => {
-    return this.state.appendCard
-      ? "modal_card appendCard"
-      : "modal_card hideCard";
-  };
-  handleClose = () => {
-    this.setState({ cardData: [], appendCard: false });
-  };
-  render() {
-    let handleClick = (e) => {
-      let cardData = this.state.beachData.find(
-        (data) => data.id == e.target.id
-      );
-      this.setState({ cardData: [cardData], appendCard: true });
-    };
-    return (
-      <React.Fragment>
-        <Modal
-          cardData={this.state.cardData}
-          handleClose={this.handleClose}
-          showClasses={this.showClasses}
-        />
-        <Cart beachData={this.state.beachData} handleClick={handleClick} />
-      </React.Fragment>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			beachData: Data,
+			cardData: [],
+			appendCard: false,
+		};
+	}
+	showClasses = () => {
+		return this.state.appendCard
+			? "modal_card appendCard"
+			: "modal_card hideCard";
+	};
+	handleClose = () => {
+		this.setState({ cardData: [], appendCard: false });
+	};
+	render() {
+		let handleClick = (e) => {
+			let cardData = this.state.beachData.find(
+				(data) => data.id === parseFloat(e.target.id)
+			);
+			this.setState({ cardData: [cardData], appendCard: true });
+		};
+		return (
+			<React.Fragment>
+				<Modal
+					cardData={this.state.cardData}
+					handleClose={this.handleClose}
+					showClasses={this.showClasses}
+				/>
+				<Cart beachData={this.state.beachData} handleClick={handleClick} />
+			</React.Fragment>
+		);
+	}
 }
 export default Home;
